ffe5d4da7e1cf40b746458a959fb784aea6363dc
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-21 22:09:27 +0500

    exercise 1.9 done

--- src/Statistics.js
+++ src/Statistics.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export default function Statistics({good,neutral,bad,avg,total}) {
-  return (
-    <div>
+export default function Statistics({ good, neutral, bad, avg, total }) {
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <h2>No Feedbacks Given</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
         <h1>Statistics</h1>
         <p>good {good} </p>
         <p>Neutral {neutral} </p>
         <p>bad {bad} </p>
         <p>Total {total} </p>
-        <p>Average {avg/total} </p>
-        <p>Positive percentage {(good/total)*100} % </p>
-     
-    </div>
-  )
+        <p>Average {avg / total} </p>
+        <p>Positive percentage {(good / total) * 100} % </p>
+      </div>
+    );
+  }
 }
