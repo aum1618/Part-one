0b3a1fb9202760fe4bde78b84025572dea9888c5
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-21 18:12:36 +0500

    exercise 1.1 done

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Content(props) {
+  return (
+    <>
+    <div><p>{props.part1} {props.exercises1} </p></div>
+    <div><p>{props.part2} {props.exercises2} </p></div>
+    <div><p>{props.part3} {props.exercises3} </p></div>
+  
+    </>
+  )
+}
