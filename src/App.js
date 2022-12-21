9cf41e9700ca116cd279caeecc7ac4b97df6c6e7
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-21 20:42:02 +0500

    exercise 1.3 done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Total from "./Total"
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name:'Fundamentals of React',
+    exercise : 10
+  }
+  const part2 = {
+    name:'Using props to pass data',
+    exercise : 7
+  }
+  const part3 = {
+    name:'state of a component',
+    exercise : 14
+  }
   return (
     <div>
       <Header header={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercise} exercises2={part2.exercise} exercises3={part3.exercise} />
+      <Total exercises1={part1.exercise} exercises2={part2.exercise} exercises3={part3.exercise} />
     </div>
 
    
