5a7d89f5eaaa9224ed74fcada5609756dfab4a02
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-21 21:47:56 +0500

    exercise 1.6 done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import Content from "./Content"
-import Header from "./Header"
-import Total from "./Total"
+import { useState } from 'react'
 
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const goodclickhandler=()=>{
+    setGood(good+1)
+
+  }
+  const neutralclickhandler=()=>{
+    setNeutral(neutral+1)
+
+  }
+  const badclickhandler=()=>{
+    setBad(bad+1)
+
   }
 
   return (
     <div>
-      <Header header={course.name} />
-      <Content part1={course.parts[0].name} part2={course.parts[1].name} part3={course.parts[2].name} exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises} />
-      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises} />
+      <h1>Give Feedback</h1>
+      <button onClick={goodclickhandler} >good</button>
+      <button onClick={neutralclickhandler} >Neutral</button>
+      <button onClick={badclickhandler} >bad</button>
+     <h1>Statistics</h1>
+     <p>good {good} </p>
+     <p>Neutral {neutral} </p>
+     <p>bad {bad} </p>
+     
+      
     </div>
-
-   
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Part.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Part(props) {
-  return (
-    <div>
-        <p>{props.name} {props.exercise} </p>
-    </div>
-  )
-}
